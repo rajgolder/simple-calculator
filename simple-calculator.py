6566d0879d4e6944d36e40a510eec787f793a313
Raj <raj.golder24@gmail.com>
2026-06-15 22:15:50 +0530

    update_1.1
    Added while loop and made slight improvement to the code.

--- simple-calculator.py
+++ simple-calculator.py
@@ -1,18 +1,35 @@
-print("Enter first number:")
-num1 = float(input())
-print("Enter second number:")
-num2 = float(input())
-print("Enter operator (+, -, *, /):")
-operator = input()
+while True:
+    print("Enter first number (or type 'done' to exit):")
+    first_input = input()
 
-if operator == "+":
-    result = num1 + num2
-elif operator == "-":
-    result = num1 - num2
-elif operator == "*":
-    result = num1 * num2
-elif operator == "/":
-    if num2 != 0:
-        result = num1 / num2
-print("Result:", result)
-#test git
+    if first_input.lower() == "done":
+        print("Calculator closed.")
+        break
+
+    num1 = float(first_input)
+
+    print("Enter second number:")
+    num2 = float(input())
+
+    print("Enter operator:")
+    operator = input()
+
+    if operator == "+":
+       result = num1 + num2
+    elif operator == "-":
+       result = num1 - num2
+    elif operator == "*":
+        result = num1 * num2
+    elif operator == "/":
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            print("Error: Division by zero")
+            result = None
+    
+    else:
+        print("Invalid operator")
+        result = None
+
+    if result is not None:
+        print("Result:", result)
